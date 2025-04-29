b11e2e6cdade5c6f265f014aef6191b49a0221c6
Edcorp844 <edsonchan6@gmail.com>
2025-04-29 12:55:31 +0300

    cors fix

--- src/utils/news_api/newsapi-config.ts
+++ src/utils/news_api/newsapi-config.ts
@@ -5,5 +5,5 @@ const NEWS_API_KEY = '8468d3e24f614f959062cd9645b04e9d' ;
 
 export const newsAPI = initNewsAPI(NEWS_API_KEY, {
   // Optional: Add CORS proxy URL if needed
-  // corsProxyUrl: 'https://your-cors-proxy.com/'
+  corsProxyUrl: 'https://cors-anywhere.herokuapp.com/'
 });
