7d44725fbcf7da5bf29ace9e3f3cec823dba69d4
Frost Edson <edsonchan6@gmail.com>
2025-04-29 15:05:46 +0300

    Update newsapi-config.ts

--- src/utils/news_api/newsapi-config.ts
+++ src/utils/news_api/newsapi-config.ts
@@ -5,5 +5,5 @@ const NEWS_API_KEY = '8468d3e24f614f959062cd9645b04e9d';
 
 export const newsAPI = initNewsAPI(NEWS_API_KEY, {
   // Optional: Add CORS proxy URL if needed
-  //corsProxyUrl: 'https://cors-anywhere.herokuapp.com/'
+  corsProxyUrl: 'https://corsproxy.io/?'
 });
